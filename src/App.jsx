1765ad0f65b6b5acc06e6bce7824df8fba303c6c
Juan Parra <juancasomoza@hotmail.com>
2024-09-26 20:09:06 -0500

    Use Api Rest con Axio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { ApiCoffeCarruselBasic } from "./components/ApiCoffeCarruselBasic";
+
+function App() {
+  return (
+    <div>
+      <ApiCoffeCarruselBasic />
+    </div>
+  );
+}
+
+export default App;
