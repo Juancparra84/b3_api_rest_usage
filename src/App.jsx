93b873720a16cc56b5b89292565c4ed6813f09ff
Juan Parra <juancasomoza@hotmail.com>
2024-09-27 19:39:36 -0500

    Galeria gatitos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import { ApiCoffeCarruselBasic } from "./components/ApiCoffeCarruselBasic";
+//import { ApiCoffeCarruselBasic } from "./components/ApiCoffeCarruselBasic";
+import { CatGalleryFetch } from "./components/CatGalleryFetch";
 
 function App() {
   return (
     <div>
-      <ApiCoffeCarruselBasic />
+      <CatGalleryFetch />
     </div>
   );
 }
